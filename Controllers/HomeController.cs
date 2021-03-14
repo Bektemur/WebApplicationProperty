49a30a97fcbaa5a984cf901666f41f848b5058aa
ash.magsumzianov <magsumzianov@greenmoney.ru>
2021-03-14 21:15:47 +0700

    Правка фильтра для поиска improvments

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,16 +22,16 @@ namespace WebApplicationProperty.Controllers
             _context = context;
 
         }
-        public IActionResult Search(int[] selectedBasic, int page = 1, int take = 25, int id = 0)
+        public async Task<IActionResult> Search(int[] selectedBasic, int page = 1, int take = 25, int id = 0)
         {
-            IndexViewModel indexViewModel = GetIndexViewModel(selectedBasic,"", page, take, id);
+            IndexViewModel indexViewModel = await GetIndexViewModel(selectedBasic, "", page, take, id);
             return View(indexViewModel);
         }
 
         [HttpPost]
-        public IActionResult SearchResult(int[] selectedBasic, string search, int page = 1, int take = 25, int id = 0)
+        public async Task<IActionResult> SearchResult(int[] selectedBasic, string search, int page = 1, int take = 25, int id = 0)
         {
-            IndexViewModel indexViewModel = GetIndexViewModel(selectedBasic, search, page, take, id);
+            IndexViewModel indexViewModel = await GetIndexViewModel(selectedBasic, search, page, take, id);
             return PartialView("_PropertySearchPartial", indexViewModel);
         }
 
@@ -64,10 +64,9 @@ namespace WebApplicationProperty.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-        public IActionResult Details(int[] selectImropments, int id, int page = 1, int take = 25)
+        public async Task<IActionResult> Details(int[] selectImropments, int id, int page = 1, int take = 25)
         {
-
-            IndexViewModel indexViewModel = GetIndexViewModel(selectImropments,"", page, take, id);
+            IndexViewModel indexViewModel = await GetIndexViewModel(selectImropments, "", page, take, id);
             if (indexViewModel == null)
             {
                 return NotFound();
@@ -75,6 +74,7 @@ namespace WebApplicationProperty.Controllers
 
             return View(indexViewModel);
         }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Details(int[] selectedImprovments, Contact contact, string Name, string Email, string Phone, string Description, int id, int page = 1, int take = 25)
@@ -90,7 +90,7 @@ namespace WebApplicationProperty.Controllers
                 _context.Contacts.Add(contact);
                 await _context.SaveChangesAsync();
             }
-            IndexViewModel indexViewModel = GetIndexViewModel(selectedImprovments,"", page, take, id);
+            IndexViewModel indexViewModel = await GetIndexViewModel(selectedImprovments, "", page, take, id);
             if (indexViewModel == null)
             {
                 return NotFound();
@@ -102,7 +102,7 @@ namespace WebApplicationProperty.Controllers
             return View();
         }
 
-        public IndexViewModel GetIndexViewModel(int[] selectedImprovments, string search, int page = 1, int take = 25, int id = 0)
+        public async Task<IndexViewModel> GetIndexViewModel(int[] selectedImprovments, string search, int page = 1, int take = 25, int id = 0)
         {
             var propertyById = id <= 0 ? null : _context.Properties
                .Include(p => p.Project)
@@ -111,22 +111,20 @@ namespace WebApplicationProperty.Controllers
                .Include(p => p.Improvements).Include(p => p.FileSystemModels)
                .FirstOrDefault(m => m.PropertyId == id);
 
-            var query = _context.Properties.Include(x => x.FileSystemModels).Include(x=>x.Improvements).AsQueryable();
+            var query = _context.Properties.Include(x => x.FileSystemModels).Include(x => x.Improvements).AsQueryable();
             if (!string.IsNullOrWhiteSpace(search))
             {
                 query = query.Where(v => v.Name.Contains(search)).AsQueryable();
             }
-            if (selectedImprovments.Count() != 0)
+            if (selectedImprovments.Length > 0)
             {
-
-                query = query.Where(u => selectedImprovments.Any(d => u.Improvements.Any(x => x.ImprovementId == d))).AsQueryable();
-
+                query = query.Where(u => u.Improvements.Any(x => selectedImprovments.Contains(x.ImprovementId))).AsQueryable();
             }
-            //var propertyList = query.Skip(page - 1).Take(take).ToList();
+            var propertyList = await query.Skip((page - 1) * take).Take(take).ToListAsync();
 
             IndexViewModel indexViewModel = new IndexViewModel()
             {
-                ListProperty = query,
+                ListProperty = propertyList,
                 Improvements = _context.Improvements.ToList(),
                 Page = page,
                 Take = take,
